59d31c9fa9c98a5d189e80043daad04eb8397a31
Charly <carlosecimino@gmail.com>
2016-11-29 23:45:10 -0300

    Proyecto terminado

---
+++ src/Compilador/Consola.java
@@ -0,0 +1,16 @@
+package Compilador;
+
+import java.util.Scanner;
+
+public class Consola {
+
+    private static final Scanner ENTRADA = new Scanner(System.in);
+
+    public static String ingresar() {
+        return ENTRADA.nextLine();
+    }
+
+    public static void mostrar(String s) {
+        System.out.print(s);
+    }
+}
